caf65e0430611929c888ad0ae05fe37525ddf0f4
Jociel Schultz <jocielschultz@gmail.com>
2023-04-18 16:44:22 -0300

    versão adminlte

--- main.js
+++ main.js
@@ -1,15 +1,150 @@
-function collapse(id) {
+var localeBR = "pt-BR";
+var optionsBR = {
+  style: "currency",
+  currency: "BRL",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+};
+var formatterBR = new Intl.NumberFormat(localeBR, optionsBR);
+
+async function buscarValorDolar() {
+  /*var myHeaders = new Headers();
+  myHeaders.append("apikey", "RU9l71v1JT8PMyReZ1dK87T001CeCMhV");
+
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+    headers: myHeaders,
+  };
+  */
+  //https://economia.awesomeapi.com.br/last/USD-BRL
+  // compra = bid
+
+  //https://docs.awesomeapi.com.br/api-de-moedas
+
+  return fetch("https://economia.awesomeapi.com.br/last/USD-BRL")
+    .then((response) => response.json())
+    .then((result) => {
+      const valorDolar = result.USDBRL.bid;
+      document.querySelector("#dolar").value = valorDolar;
+      document.querySelector(
+        "#dolar_hoje"
+      ).innerHTML = `Dólar Hoje: ${formatterBR.format(valorDolar)}`;
+
+      return valorDolar;
+    })
+    .catch((error) => console.log("error", error));
+}
+
+buscarValorDolar();
+
+function abrir(id) {
   var cx = document.querySelector(id);
-  if (cx.style.display == "block") {
-    cx.style.display = "none";
+  cx.style.display = "block";
+}
+
+function fechar(id) {
+  if (id == "#cx2") {
+    var cx3 = document.querySelector("#cx3");
+    cx3.style.display = "none";
+
+    var cx4 = document.querySelector("#cx4");
+    cx4.style.display = "none";
+  }
+
+  if (id == "#cx3") {
+    var cx4 = document.querySelector("#cx4");
+    cx4.style.display = "none";
+  }
+
+  var cx = document.querySelector(id);
+  cx.style.display = "none";
+}
+
+function validarCampos(id) {
+  var cx_altura = document.querySelector(`#${id}_dim1`);
+  var cx_largura = document.querySelector(`#${id}_dim2`);
+  var cx_comprimento = document.querySelector(`#${id}_dim3`);
+  var cx_peso = document.querySelector(`#${id}_peso`);
+
+  var valido = true;
+
+  if (cx_altura.value == "" || Number(cx_altura.value) == 0) {
+    cx_altura.classList.add("is-invalid");
+    valido = false;
   } else {
-    cx.style.display = "block";
+    cx_altura.classList.remove("is-invalid");
+  }
+
+  if (cx_largura.value == "" || Number(cx_largura.value) == 0) {
+    cx_largura.classList.add("is-invalid");
+    valido = false;
+  } else {
+    cx_largura.classList.remove("is-invalid");
+  }
+
+  if (cx_comprimento.value == "" || Number(cx_comprimento.value) == 0) {
+    cx_comprimento.classList.add("is-invalid");
+    valido = false;
+  } else {
+    cx_comprimento.classList.remove("is-invalid");
+  }
+
+  if (cx_peso.value == "" || Number(cx_peso.value) == 0) {
+    cx_peso.classList.add("is-invalid");
+    valido = false;
+  } else {
+    cx_peso.classList.remove("is-invalid");
+  }
+
+  return valido;
+}
+
+function retornarCaixasVisiveis() {
+  var caixas = ["cx1"];
+
+  var cx2 = document.querySelector("#cx2");
+  if (cx2.style.display == "block") {
+    caixas.push("cx2");
+  }
+
+  var cx3 = document.querySelector("#cx3");
+  if (cx3.style.display == "block") {
+    caixas.push("cx3");
+  }
+
+  var cx4 = document.querySelector("#cx4");
+  if (cx4.style.display == "block") {
+    caixas.push("cx4");
+  }
+
+  return caixas;
+}
+
+function calcularFreteFinal() {
+  var caixas = retornarCaixasVisiveis();
+
+  var valoresFrete = [];
+
+  caixas.forEach((cx) => {
+    var valorFrete = calcularFreteIndividual(cx);
+    if (valorFrete != false) {
+      valoresFrete.push(valorFrete);
+    }
+  });
+
+  if (valoresFrete.length > 0) {
+    exibirTabelaFinal(valoresFrete);
   }
 }
 
-function calcularFrete() {
+function exibirTabelaFinal(valoresFrete) {
+  var tabela = document.querySelector("#footer");
+  tabela.style.display = "block";
+
   //inicializacao das variáveis
   const dolar = Number(document.querySelector("#dolar").value);
+  //const dolar = 5;
 
   // formatar em dólar e real
   var locale = "en-US";
@@ -21,147 +156,140 @@ function calcularFrete() {
   };
   var formatter = new Intl.NumberFormat(locale, options);
 
-  var localeBR = "pt-BR";
-  var optionsBR = {
-    style: "currency",
-    currency: "BRL",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  };
-  var formatterBR = new Intl.NumberFormat(localeBR, optionsBR);
+  var valorTotal = 0;
+  document.getElementById("tabela").getElementsByTagName("tbody")[0].remove();
 
-  // pegar valores dos input
-  var cx1_dim1 = Number(document.querySelector("#cx1_dim1").value);
-  var cx1_dim2 = Number(document.querySelector("#cx1_dim2").value);
-  var cx1_dim3 = Number(document.querySelector("#cx1_dim3").value);
-  var cx1_soma_lados = cx1_dim1 + cx1_dim2 + cx1_dim3;
-  var cx1_peso = Number(document.querySelector("#cx1_peso").value);
-  var cx1_fator_multiplicador_frete = cx1_soma_lados * cx1_peso;
+  document.getElementById("tabela").createTBody();
 
-  var cx2_dim1 = document.querySelector("#cx2_dim1").value;
-  var cx2_dim2 = document.querySelector("#cx2_dim2").value;
-  var cx2_dim3 = document.querySelector("#cx2_dim3").value;
-  var cx2_peso = document.querySelector("#cx2_peso").value;
-  var cx2_soma_lados = cx2_dim1 + cx2_dim2 + cx2_dim3;
-  var cx2_fator_multiplicador_frete = cx2_soma_lados * cx2_peso;
+  var tbodyRef = document
+    .getElementById("tabela")
+    .getElementsByTagName("tbody")[0];
 
-  var cx3_dim1 = document.querySelector("#cx3_dim1").value;
-  var cx3_dim2 = document.querySelector("#cx3_dim2").value;
-  var cx3_dim3 = document.querySelector("#cx3_dim3").value;
-  var cx3_peso = document.querySelector("#cx3_peso").value;
-  var cx3_soma_lados = cx3_dim1 + cx3_dim2 + cx3_dim3;
-  var cx3_fator_multiplicador_frete = cx3_soma_lados * cx3_peso;
+  valoresFrete.forEach((frete) => {
+    // Insert a row at the end of table
+    var newRow = tbodyRef.insertRow();
 
-  var cx4_dim1 = document.querySelector("#cx4_dim1").value;
-  var cx4_dim2 = document.querySelector("#cx4_dim2").value;
-  var cx4_dim3 = document.querySelector("#cx4_dim3").value;
-  var cx4_peso = document.querySelector("#cx4_peso").value;
-  var cx4_soma_lados = cx4_dim1 + cx4_dim2 + cx4_dim3;
-  var cx4_fator_multiplicador_frete = cx4_soma_lados * cx4_peso;
+    // Insert a cell at the end of the row
 
-  var valor_total = 0;
-  var valor_frete = 0;
-  var valor_frete_real = 0;
-  var condicao = 0;
+    // Caixa
+    var newCellCaixa = newRow.insertCell();
+    var newTextCaixa = document.createTextNode(`Caixa ${frete.caixa}`);
+    newCellCaixa.appendChild(newTextCaixa);
 
-  //custo final importacao
-  var valor_produto = 0;
-  var custo_final_importacao = 0;
+    // Frete dólar
+    var newCellFreteDolar = newRow.insertCell();
+    var newTextFreteDolar = document.createTextNode(
+      `${formatter.format(frete.frete_dolar)}`
+    );
+    newCellFreteDolar.appendChild(newTextFreteDolar);
 
-  if (cx1_fator_multiplicador_frete > 0) {
-    condicao = calcularCondicaoCalculo(cx1_dim1, cx1_dim2, cx1_dim3, cx1_peso);
-    valor_frete = calcularValorFrete(condicao, cx1_soma_lados, cx1_peso);
+    // Dólar
+    /*var newCellDolar = newRow.insertCell();
+    var newTextDolar = document.createTextNode(`Dólar ${dolar}`);
+    newCellDolar.appendChild(newTextDolar);
+*/
+    // Frete R$
+    var newCellFreteReal = newRow.insertCell();
+    var newTextFreteReal = document.createTextNode(
+      `${formatterBR.format(frete.frete_dolar * dolar)}`
+    );
+    newCellFreteReal.appendChild(newTextFreteReal);
 
-    var cx1_frete_dolar = document.querySelector("#cx1_frete_dolar");
-    cx1_frete_dolar.innerHTML = formatter.format(valor_frete);
+    console.log(frete.frete_dolar, dolar);
+    // Condição
+    /*    var newCellCondicao = newRow.insertCell();
+    var newTextCondicao = document.createTextNode(`Condição ${frete.condicao}`);
+    newCellCondicao.appendChild(newTextCondicao);
+*/ //
+    /*var cx_frete_dolar = document.querySelector(`#${id}_frete_dolar`);
+    cx_frete_dolar.innerHTML = formatter.format(valor_frete);
 
-    var cx1_frete_real = document.querySelector("#cx1_frete_real");
+    var cx_frete_real = document.querySelector(`#${id}_frete_real`);
     valor_frete_real = valor_frete * dolar;
-    cx1_frete_real.innerHTML = formatterBR.format(valor_frete_real);
+    cx_frete_real.innerHTML = formatterBR.format(valor_frete_real);
+    */
 
-    //custo_importacao
-    custo_final_importacao = calcularCustoFinalImportacao(
-      condicao,
-      valor_frete_real
-    );
+    valorTotal += frete.frete_dolar;
+  });
 
-    var cx1_custo_final_importacao = document.querySelector(
-      "#cx1_custo_final_importacao"
-    );
-    cx1_custo_final_importacao.innerHTML = formatterBR.format(
-      custo_final_importacao
-    );
+  var totalFreteDolar = document.querySelector("#total_frete_dolar");
+  totalFreteDolar.innerHTML = `${formatter.format(valorTotal)}`;
 
-    valor_total += valor_frete;
+  var totalFreteReal = document.querySelector("#total_frete_real");
+  totalFreteReal.innerHTML = `${formatterBR.format(valorTotal * dolar)}`;
+}
+
+function calcularFreteIndividual(id) {
+  var valido = validarCampos(id);
+
+  if (valido) {
+    // pegar valores dos input
+    var cx_dim1 = Number(document.querySelector(`#${id}_dim1`).value);
+    var cx_dim2 = Number(document.querySelector(`#${id}_dim2`).value);
+    var cx_dim3 = Number(document.querySelector(`#${id}_dim3`).value);
+    var cx_peso = Number(document.querySelector(`#${id}_peso`).value);
+
+    var cx_soma_lados = cx_dim1 + cx_dim2 + cx_dim3;
+    var cx_fator_multiplicador_frete = cx_soma_lados * cx_peso;
+
+    // inicializacao variáveis
+    var valor_total = 0;
+    var valor_frete = 0;
+    var valor_frete_real = 0;
+    var condicao = 0;
+
+    //custo final importacao
+    var valor_produto = 0;
+    var custo_final_importacao = 0;
+
+    if (cx_fator_multiplicador_frete > 0) {
+      condicao = calcularCondicaoCalculo(cx_dim1, cx_dim2, cx_dim3, cx_peso);
+      valor_frete = calcularValorFrete(condicao, cx_soma_lados, cx_peso);
+
+      if (id == "cx2") {
+        valor_frete = valor_frete * 0.6;
+      }
+
+      if (id == "cx3") {
+        valor_frete = valor_frete * 0.5;
+      }
+
+      if (id == "cx4") {
+        valor_frete = valor_frete * 0.4;
+      }
+
+      /*var cx_frete_dolar = document.querySelector(`#${id}_frete_dolar`);
+      cx_frete_dolar.innerHTML = formatter.format(valor_frete);
+
+      var cx_frete_real = document.querySelector(`#${id}_frete_real`);
+      valor_frete_real = valor_frete * dolar;
+      cx_frete_real.innerHTML = formatterBR.format(valor_frete_real);
+  */
+      //custo_importacao
+      /*custo_final_importacao = calcularCustoFinalImportacao(
+        condicao,
+        valor_frete_real
+      );
+
+      var cx_custo_final_importacao = document.querySelector(
+        `#${id}_custo_final_importacao`
+      );
+      cx_custo_final_importacao.innerHTML = formatterBR.format(
+        custo_final_importacao
+      );
+        */
+
+      valor_total += valor_frete;
+
+      return {
+        caixa: id,
+        frete_dolar: valor_frete,
+        condicao: condicao,
+      };
+    }
+    return false;
   }
-
-  if (cx2_fator_multiplicador_frete > 0) {
-    condicao = calcularCondicaoCalculo(cx2_dim1, cx2_dim2, cx2_dim3, cx2_peso);
-    valor_frete = calcularValorFrete(condicao, cx2_soma_lados, cx2_peso);
-    valor_final_importacao = calcularCustoFinalImportacao(
-      condicao,
-      valor_frete
-    );
-
-    valor_frete = valor_frete * 0.6;
-
-    var cx2_frete_dolar = document.querySelector("#cx2_frete_dolar");
-    cx2_frete_dolar.innerHTML = formatter.format(valor_frete);
-
-    var cx2_frete_real = document.querySelector("#cx2_frete_real");
-    valor_frete_real = valor_frete * dolar;
-    cx2_frete_real.innerHTML = formatterBR.format(valor_frete_real);
-
-    valor_total += valor_frete;
-  }
-
-  if (cx3_fator_multiplicador_frete > 0) {
-    condicao = calcularCondicaoCalculo(cx3_dim1, cx3_dim2, cx3_dim3, cx3_peso);
-    valor_frete = calcularValorFrete(condicao, cx3_soma_lados, cx3_peso);
-    valor_final_importacao = calcularCustoFinalImportacao(
-      condicao,
-      valor_frete
-    );
-
-    valor_frete = valor_frete * 0.5;
-
-    var cx3_frete_dolar = document.querySelector("#cx3_frete_dolar");
-    cx3_frete_dolar.innerHTML = formatter.format(valor_frete);
-
-    var cx3_frete_real = document.querySelector("#cx3_frete_real");
-    valor_frete_real = valor_frete * dolar;
-    cx3_frete_real.innerHTML = formatterBR.format(valor_frete_real);
-
-    valor_total += valor_frete;
-  }
-
-  if (cx4_fator_multiplicador_frete > 0) {
-    condicao = calcularCondicaoCalculo(cx4_dim1, cx4_dim2, cx4_dim3, cx4_peso);
-    valor_frete = calcularValorFrete(condicao, cx4_soma_lados, cx4_peso);
-    valor_final_importacao = calcularCustoFinalImportacao(
-      condicao,
-      valor_frete
-    );
-
-    valor_frete = valor_frete * 0.4;
-
-    var cx4_frete_dolar = document.querySelector("#cx4_frete_dolar");
-    cx4_frete_dolar.innerHTML = formatter.format(valor_frete);
-
-    var cx4_frete_real = document.querySelector("#cx4_frete_real");
-    valor_frete_real = valor_frete * dolar;
-    cx4_frete_real.innerHTML = formatterBR.format(valor_frete_real);
-
-    valor_total += valor_frete;
-  }
-
-  // exibe valores totais
-  var frete_dolar = document.querySelector("#frete_dolar");
-  frete_dolar.innerHTML = formatter.format(valor_total);
-
-  var frete_real = document.querySelector("#frete_real");
-  valor_total_real = valor_total * dolar;
-  frete_real.innerHTML = formatterBR.format(valor_total_real);
+  return false;
 }
 
 function calcularValorFrete(condicao, soma_lados, peso) {
@@ -200,22 +328,50 @@ function calcularValorFrete(condicao, soma_lados, peso) {
 }
 
 function calcularCondicaoCalculo(dim1, dim2, dim3, peso) {
+  var alert = document.querySelector("#alert");
+  alert.style.display = "none";
+
   var soma_lados = dim1 + dim2 + dim3;
   var condicao = 0;
+
+  var message = "";
+  //Pelo menos 1 lado acima de 100cm (ate 150cm)
+  if (dim1 > 150 || dim2 > 150 || dim3 > 15) {
+    message += "Valor máximo de 150cm. <br>";
+  }
+  //soma dos lados (SL) maior que 200cm (ate 350cm)
+  if (soma_lados > 350) {
+    message += "Valor máximo da soma dos lados de 350cm.<br>";
+  }
+
+  //peso maior que 30kg (ate 40kg)
+  if (peso > 40) {
+    message += "Peso máximo permitido de 40kg";
+  }
+
+  if (message != "") {
+    alert.style.display = null;
+
+    var msg = document.querySelector("#msg");
+    msg.innerHTML = message;
+  }
 
   //Nenhum lado com valores acima ou igual a 70cm, peso igual ou inferior 30kg
   if (dim1 < 70 && dim2 < 70 && dim3 < 70 && peso <= 30) {
     condicao = 1;
+    console.log("condicao base");
   }
 
   //Pelo menos 1 lado com dimensao igual ou maior q 70cm, peso igual ou inferior 30kg
   if ((dim1 >= 70 || dim2 >= 70 || dim3 >= 70) && peso <= 30) {
     condicao = 2;
+    console.log("condicao 1");
   }
 
   //Pelo menos 1 lado acima de 100cm (ate 150cm) ou soma dos lados (SL) maior que 200cm (ate 350cm) ou peso maior que 30kg (ate 40kg)
   if (dim1 > 100 || dim2 > 100 || dim3 > 100 || soma_lados > 200 || peso > 30) {
     condicao = 3;
+    console.log("condicao limite");
   }
 
   return condicao;
